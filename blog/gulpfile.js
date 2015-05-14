ba11f16557a43e7cb3eee0a443e6ac533f5077e7
Maxim Poletaev <zenwalker2@gmail.com>
2015-05-14 19:50:10 +0300

    Initial commit

---
+++ blog/gulpfile.js
@@ -0,0 +1,20 @@
+var concat = require('gulp-concat')
+  , stylus = require('gulp-stylus')
+  , django = require('gulp-django-utils');
+
+var sources = {
+  css: ['./static/blog/*.styl']
+};
+
+module.exports = function(project, gulp) {
+  var app = new django.Application('blog', project);
+
+  app.task('css', function() {
+    app.src(sources.css)
+      .pipe(stylus())
+      .pipe(concat('styles.css'))
+      .pipe(app.dest('static/blog/build'));
+  });
+
+  app.task('default', ['css']);
+};
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp')
+  , stylus = require('gulp-stylus')
+  , concat = require('gulp-concat')
+  , django = require('gulp-django-utils');
+
+var apps = [
+  'blog'
+];
+
+project = new django.Project(gulp, apps, __dirname);
+project.discoverApps();
+
+var sources = {
+  app: {
+    js: ['./static/scripts/*.js'],
+    css: ['./static/styles/*.styl']
+  }
+};
+
+gulp.task('css', function() {
+  project.src(sources.app.css)
+    .pipe(stylus())
+    .pipe(concat('bundle.css'))
+    .pipe(project.dest('./static/build'));
+});
+
+gulp.task('js', function() {
+  project.src(sources.app.js)
+    .pipe(concat('bundle.js'))
+    .pipe(project.dest('./static/build'));
+});
+
+project.task('watch', function() {
+  gulp.watch(sources.app.css, ['css'])
+  gulp.watch(sources.app.js, ['css'])
+});
+
+project.task('default', ['css', 'js']);
